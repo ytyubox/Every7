d784b83b79bb24da583013fe69294dd69a808cdd
Tsungyu Yu <ytyubox@gmail.com>
2021-03-11 13:39:12 +0800

    move powi into combination, DRY computatoin

--- Tests/Every7Tests/CombinationTests.swift
+++ Tests/Every7Tests/CombinationTests.swift
@@ -14,30 +14,43 @@ enum Combination {
         decision(edge.description)
     }
 
-    static func decision<S: StringProtocol>(_ input: S) -> Int {
-        if atol(input.description) <= 10 {
-            return single(atol(input.description))
+    private static func decision<S: StringProtocol>(_ input: S) -> Int {
+        let value = atol(input.description)
+        if value <= 10 {
+            return single(value)
         }
+        let maxExponent = input.count - 1
+        /*
+         for example:
+            123 == 1.23 * 10^2 where maxExponent == 2 == 3 - 1 == count - 1
+         */
         guard let first = input.first?.wholeNumberValue else { return 0 }
-        let base1 = powi(10, input.count - 2)
+        let base1 = powi(10, maxExponent - 1)
         if first == 1 {
             return decision(base1.description) * 9 + base1
         }
-        
+
         let base = base1 * 10
         switch first {
-        case 2 ... 6: return decision(base.description) * first + decision(input.dropFirst())
+        case ...6: return decision(base.description) * first + decision(input.dropFirst())
         case 7: return decision(base.description) * first + atol(input.dropFirst().description) + 1
-        case 8 ... 9: return decision((base * first - 1).description) + decision(input.dropFirst())
-        default:
-            fatalError()
+        default: return decision((base * first - 1).description) + decision(input.dropFirst())
         }
     }
 
-    static func single(_ edge: Int) -> Int {
+    private static func single(_ edge: Int) -> Int {
         if edge < 7 { return 0 }
         if edge <= 10 { return 1 }
         return 0
+    }
+
+    static func powi(_ base: Int, _ up: Int) -> Int {
+        var r = 1
+        guard up >= 0 else { return 0 }
+        for _ in 0 ..< up {
+            r = base &* r
+        }
+        return r
     }
 }
 
@@ -52,28 +65,47 @@ final class CombinationTests: XCTestCase {
     func test7() {
         assertWithBruteForce(g, target: 7)
     }
+
     func test10() {
         assertWithBruteForce(g, target: 10)
     }
+
     func test20() {
         assertWithBruteForce(g, target: 20)
     }
+
     func test70() {
         assertWithBruteForce(g, target: 70)
     }
+
     func test100() {
         assertWithBruteForce(g, target: 100)
     }
+
     func test699() {
         assertWithBruteForce(g, target: 699)
     }
+
     func test800() {
         assertWithBruteForce(g, target: 800)
     }
+
     func test1000() {
         assertWithBruteForce(g, target: 1000)
     }
+
     func testIntMax() {
-        XCTAssertEqual(g(Int.max), 7984850453867298887)
+        let result = 7_984_850_453_867_298_887
+        let start = CFAbsoluteTimeGetCurrent()
+        XCTAssertEqual(g(Int.max), result)
+        let finished = CFAbsoluteTimeGetCurrent()
+        let costOfTime = finished - start
+        XCTAssertLessThan(costOfTime, 2)
+    }
+
+    func testpowi() {
+        XCTAssertEqual(Combination.powi(10, 2), 100)
+        XCTAssertEqual(Combination.powi(10, 0), 1)
+        XCTAssertEqual(Combination.powi(10, -1), 0)
     }
 }
--- Tests/Every7Tests/Every7Tests.swift
+++ Tests/Every7Tests/Every7Tests.swift
@@ -1,24 +1,44 @@
 import XCTest
-enum BruteForce {
-    static func g(_ edge: Int) -> Int {
-        (1...edge).map(\.description).filter{$0.contains("7")}.count
-    }
-}
-
-
 
 final class Every7Tests: XCTestCase {
-    func testBruteForce() {
+    func testBruteForceGeneralCase() {
         let g = BruteForce.g
+        XCTAssertEqual(g(6), 0)
         XCTAssertEqual(g(7), 1)
+        XCTAssertEqual(g(10), 1)
+        XCTAssertEqual(g(17), 2)
+        XCTAssertEqual(g(17), g(10) + g(7))
         XCTAssertEqual(g(20), 2)
+        XCTAssertEqual(g(27), 3)
+        XCTAssertEqual(g(27), g(10) * 2 + g(7))
+        XCTAssertEqual(g(69), 7)
+        XCTAssertEqual(g(69), g(10) * 6 + g(7))
         XCTAssertEqual(g(70), 8)
-        XCTAssertEqual(g(100) ,19)
-        XCTAssertEqual(g(699) ,133)
-        XCTAssertEqual(g(800) ,233)
-        XCTAssertEqual(271, 19 * 9 + 100)
-        XCTAssertEqual(g(1000), g(100) * 9 + 100)
+        XCTAssertEqual(g(70), g(10) * 6 + g(7) + (70 % 10 + 1))
+        XCTAssertEqual(g(100), 19)
+        XCTAssertEqual(g(100), g(10) * 9 + 10)
+        XCTAssertEqual(g(200), g(100) * 1 + g(99))
+        XCTAssertEqual(g(600), g(100) * 6)
+        XCTAssertEqual(g(600), g(100) * 5 + g(600 - 500))
+        XCTAssertEqual(g(699), 133)
+        XCTAssertEqual(g(699), g(100) * 6 + g(699 - 600))
+        XCTAssertEqual(g(739), g(699) + 40)
+        XCTAssertEqual(g(739), g(100) * 7 + 40)
+        XCTAssertEqual(g(800), 233)
         XCTAssertEqual(g(800), g(100) * 7 + 100)
+        XCTAssertEqual(g(900), g(100) * 8 + 100)
+        XCTAssertEqual(271, 19 * 9 + 100)
+
+        XCTAssertEqual(g(800), g(100) * 7 + 100)
+        XCTAssertEqual(g(823), g(800) + g(23))
+
+        XCTAssertEqual(g(923), g(899) + g(23))
+        XCTAssertEqual(g(923), g(900) + g(23))
+
+        XCTAssertEqual(g(1000), g(100) * 9 + 100)
         XCTAssertEqual(g(1000), 271)
+        XCTAssertEqual(g(7890), g(6999) + 891)
+        XCTAssertEqual(g(7890), g(1000) * 6 + g(999) + 891)
+        XCTAssertEqual(g(7890), g(1000) * 7 + 891)
     }
 }
