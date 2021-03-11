5bddad590ec52a82d769d1777ed3c453cbb20695
Tsungyu Yu <ytyubox@gmail.com>
2021-03-11 10:28:37 +0800

    add brute force

--- Tests/Every7Tests/Every7Tests.swift
+++ Tests/Every7Tests/Every7Tests.swift
@@ -1,15 +1,24 @@
 import XCTest
-@testable import Every7
+enum BruteForce {
+    static func g(_ edge: Int) -> Int {
+        (1...edge).map(\.description).filter{$0.contains("7")}.count
+    }
+}
+
+
 
 final class Every7Tests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Every7().text, "Hello, World!")
+    func testBruteForce() {
+        let g = BruteForce.g
+        XCTAssertEqual(g(7), 1)
+        XCTAssertEqual(g(20), 2)
+        XCTAssertEqual(g(70), 8)
+        XCTAssertEqual(g(100) ,19)
+        XCTAssertEqual(g(699) ,133)
+        XCTAssertEqual(g(800) ,233)
+        XCTAssertEqual(271, 19 * 9 + 100)
+        XCTAssertEqual(g(1000), g(100) * 9 + 100)
+        XCTAssertEqual(g(800), g(100) * 7 + 100)
+        XCTAssertEqual(g(1000), 271)
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import Every7Tests
-
-var tests = [XCTestCaseEntry]()
-tests += Every7Tests.allTests()
-XCTMain(tests)
