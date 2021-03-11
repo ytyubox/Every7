e2fb9501ec83a8e9c7b6f3390e07ee77b99fa614
Tsungyu Yu <ytyubox@gmail.com>
2021-03-11 13:38:18 +0800

    format code

--- Tests/Every7Tests/BruteForceTests.swift
+++ Tests/Every7Tests/BruteForceTests.swift
@@ -1,22 +1,20 @@
 //
-/* 
+/*
  *		Created by 游宗諭 in 2021/3/11
- *		
+ *
  *		Using Swift 5.0
- *		
+ *
  *		Running on macOS 11.2
  */
-
 
 import Foundation
 enum BruteForce {
     static func g(_ edge: Int) -> Int {
         var count = 0
-        for i in (1...edge) {
-            if i.description.contains("7") {count += 1}
+        for i in 1 ... edge {
+            if i.description.contains("7") { count += 1 }
         }
         return count
-        
     }
 }
 
